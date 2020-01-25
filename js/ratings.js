d278b1d1433de8e830640d0089a83c71b3fff72f
IoanaCiuca <ioana.ciuca2@gmail.com>
2020-01-25 18:44:47 +0200

    Completed module

--- js/ratings.js
+++ js/ratings.js
@@ -15,7 +15,7 @@ const collect_ratings = () => {
         ratings.sum += parseInt(element.value) * rating;
     });
 
-    if (ratings.count) {
+    if (ratings.count !== 0) {
         ratings.average = ratings.sum / ratings.count;
     }
     return ratings;
